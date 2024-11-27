8312b50defa0d8cf44385341c11e6c797c09a251
Brys <35676267+brys0@users.noreply.github.com>
2024-11-26 22:34:07 -0600

    fix: make middleware run once, not on every page nav

--- middleware/authenticated.ts
+++ middleware/authenticated.ts
@@ -1,47 +1,47 @@
 import type { UserDto } from '@jellyfin/sdk/lib/generated-client'
 
-export default defineNuxtRouteMiddleware(async () => {
-  const serverStore = useServerStore()
-  const server = useCookie('server')
+export default defineNuxtRouteMiddleware(() => {
+  callOnce(async () => {
+    const serverStore = useServerStore()
+    const server = useCookie('server')
 
-  if (window?.wails) {
-    await useNativeStorage()
-  }
-
-  if (!server.value && !serverStore.url) return setup()
-
-  const authentication = useCookie('authentication')
-  const user = useCookie('user')
-  const authStore = useAuthenticationStore()
-  const userStore = useUserStore()
-  const l = useLoggerStore()
-
-  const auth = authentication.value as unknown as {
-    _header: {
-      client: string
-      device: string
-      deviceID: string
-      version: string
-      authorization: string
+    if (window?.wails) {
+      await useNativeStorage()
     }
-    loggedIn: boolean
-  }
 
-  if (!auth) return login()
+    if (!server.value && !serverStore.url) return setup()
 
-  authStore._header = auth._header
-  authStore.loggedIn = auth.loggedIn
+    const authentication = useCookie('authentication')
+    const user = useCookie('user')
+    const authStore = useAuthenticationStore()
+    const userStore = useUserStore()
+    const l = useLoggerStore()
 
-  const userVal = user.value as unknown as { user: UserDto }
-  if (!userVal) return
+    const auth = authentication.value as unknown as {
+      _header: {
+        client: string
+        device: string
+        deviceID: string
+        version: string
+        authorization: string
+      }
+      loggedIn: boolean
+    }
 
-  userStore.user = userVal.user
+    if (!auth) return login()
 
-  callOnce(() => {
+    authStore._header = auth._header
+    authStore.loggedIn = auth.loggedIn
+
+    const userVal = user.value as unknown as { user: UserDto }
+    if (!userVal) return
+
+    userStore.user = userVal.user
+
     l.$log({
       type: 'info',
       location: 'Middleware',
-      message: `You're authenticated! Welcome back ${userStore?.user?.Name} :)`,
+      message: `You're authenticated! Welcome back ${userStore.user.Name} :)`,
     })
   })
 })
